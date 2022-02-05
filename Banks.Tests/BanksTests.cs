7e0879605c130de62d782a07cbc00939907e0f14
jizapika <78953116+jizapika@users.noreply.github.com>
2022-02-05 13:31:38 +0300

    chore: changed dotnet version

---
+++ Banks.Tests/BanksTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Banks.Tests
+{
+    public class BanksTests {
+
+        [Test]
+        public void NewTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
